333d0572d9571ad34ddf6afd2619aeb4ad12ab15
King Louie <nicoolas.t@gmail.com>
2015-05-03 22:53:13 +0200

    minor changes

--- linux/bin/suspend-timeout.sh
+++ linux/bin/suspend-timeout.sh
@@ -2,7 +2,10 @@
 
 secs=10
 [ -n "$1" ] && secs=$1
-
-echo "Suspending <$(hostname)> in $secs seconds"
+_log() {
+	echo "$*"
+	logger "$*"
+}
+_log "Suspending <$(hostname)> in $secs seconds"
 bash -c "(sleep $secs ; echo 'Suspending NOW!' ; sudo pm-suspend)" &
 
