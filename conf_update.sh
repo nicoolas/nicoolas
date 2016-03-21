8c1250428be58bc854486b5d47f8f95fe99e5e33
Nicoolas <nicoolas.t@gmail.com>
2016-03-21 16:19:34 +0100

    readme update

---
+++ conf_update.sh
@@ -0,0 +1,3 @@
+wget --no-check-certificate https://github.com/nicoolas/nicoolas/raw/master/linux/conf/_.tmux.conf -O ~/.tmux.conf
+wget --no-check-certificate https://github.com/nicoolas/nicoolas/raw/master/linux/conf/_.vimrc -O ~/.vimrc
+
