4f6cb0b264026a8375fa96fe327999c2ea022699
Nicoolas <nicoolas.t@gmail.com>
2016-09-14 23:29:01 +0200

    added disk_usage_alert.sh

---
+++ linux/bin/disk_usage_alert.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+alert_level=3G
+free_space=$(df -H | grep ' /$' | tr '%' ' ' | awk '{ print $4 }')
+cat <<EOS
+
+  Disk free space alert: $alert_level
+  Disk free space available: $free_space
+
+EOS
+
+if echo -e "$alert_level\n$free_space" | sort --check=silent -h
+then
+  msg_subject="[$(hostname)] Disk space alert: $free_space"
+  msg_contents="Server: $(hostname)\nDate: $(date)\nDisk space available: $free_space"
+  echo "$msg_contents"
+  echo "$msg_contents" | mail -s "$msg_subject" nicoolas.t+server@gmail.com
+fi
+
