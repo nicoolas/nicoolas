8cc85ff4489b1f611dc2e8b886baf3b0c01b38d7
Nicoolas <nicoolas.t@gmail.com>
2019-03-21 22:33:23 +0000

    changes in disk_usage_alert.sh

--- linux/bin/disk_usage_alert.sh
+++ linux/bin/disk_usage_alert.sh
@@ -1,18 +1,62 @@
 #!/bin/sh
-alert_level=3G
-free_space=$(df -H | grep ' /$' | tr '%' ' ' | awk '{ print $4 }')
-cat <<EOS
+conf=$(dirname $0)/$(basename $0 .sh).conf
+echo "Conf: $conf"
+[ -r $conf ] || exit 1
+. $conf
 
-  Disk free space alert: $alert_level
-  Disk free space available: $free_space
+timestamp_file=/tmp/$(basename $0).timestamp
+
+f_K2G() {
+	echo "scale=2; $1/1024/1024" | bc 
+}
+
+f_log() {
+	logger --stderr "$*"
+	echo "$(date) - $*" >> $log_file
+}
+
+f_check_timestamp() {
+	[ -s $timestamp_file ] || echo 0 > $timestamp_file
+	head -n 1 $timestamp_file | grep -q '^[0-9][0-9]*$' || echo 0 > $timestamp_file
+	local ts=$(head -n 1 $timestamp_file)
+	local now=$(date "+%s")
+
+	[ $((now - ts)) -lt $dont_alert_for_sec ] && return 1
+	echo $now>$timestamp_file
+	return 0
+}
+
+total_kb=$(df  $mount_point | sed -n '2p' | tr '%' ' ' | awk '{ print $2 }')
+free_kb=$(df $mount_point | sed -n '2p' | tr '%' ' ' | awk '{ print $4 }')
+
+alert_gb="$(f_K2G $alert_kb)GB"
+total_gb="$(f_K2G $total_kb)GB"
+free_gb="$(f_K2G $free_kb)GB"
+
+msg_subject="[$(hostname)] Disk space alert: $free_gb"
+msg_contents=$(cat <<-EOS
+
+ Server: $(hostname)
+ Date: $(date)
+ Disk space available: $free_gb/$total_gb
+ Alert below: $alert_gb
 
 EOS
+)
 
-if echo -e "$free_space\n$alert_level\n" | sort --check=silent -h
+printf "$msg_contents\n\n"
+extra_msg=""
+if [ $free_kb -lt $alert_kb ]
 then
-  msg_subject="[$(hostname)] Disk space alert: $free_space"
-  msg_contents="Server: $(hostname)\nDate: $(date)\nDisk space available: $free_space"
-  echo "$msg_contents"
-  echo "$msg_contents" | mail -s "$msg_subject" nicoolas.t+server@gmail.com
+	if f_check_timestamp
+	then
+		extra_msg=" -> raise alert"
+		[ -n "$email" ] && echo "$msg_contents" | mail -s "$msg_subject" $email
+	else
+		extra_msg=" -> inhibate alert"
+	fi
 fi
+f_log "Disk space check: $free_gb/$alert_gb/$total_gb$extra_msg"
+
+echo
 
